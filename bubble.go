74c9760689c79018a4fdc892c0d3286e76d6be07
Zachary Weeden <zdw7287@rit.edu>
2020-03-14 01:59:33 -0400

    snake to camelcase and gofmt

--- bubble.go
+++ bubble.go
@@ -6,36 +6,35 @@ import (
 	"time"
 )
 
-
 func main() {
-	length_of_array := 10
-	array := make([]int, length_of_array)
+	lengthOfArray := 10
+	array := make([]int, lengthOfArray)
 	rand.Seed(time.Now().UTC().UnixNano())
 	for i := range array {
-		array[i] = rand.Intn(10+1) // 0-10 - rand.Intn [0, n)
+		array[i] = rand.Intn(10 + 1) // 0-10 - rand.Intn [0, n)
 	}
 	fmt.Println(array)
 	start := time.Now()
-	for passes := 0; passes < length_of_array; passes++ {
-		array_changed := false
-		for i := 0; i < length_of_array-passes; i++ {
+	for passes := 0; passes < lengthOfArray; passes++ {
+		arrayChanged := false
+		for i := 0; i < lengthOfArray-passes; i++ {
 			//fmt.Printf("passes: %d, i: %d\n", passes, i)
-			if i == length_of_array - 1 {
+			if i == lengthOfArray-1 {
 				break
 			}
 			v1 := array[i]
 			v2 := array[i+1]
 			if v1 > v2 {
-				array_changed = true
+				arrayChanged = true
 				array[i+1] = v1
-				array[i]  = v2
+				array[i] = v2
 			}
 		}
-		if !array_changed {
+		if !arrayChanged {
 			break
 		}
 	}
-	total_time := time.Since(start)
+	totalTime := time.Since(start)
 	fmt.Println(array)
-	fmt.Printf("sort took %s to sort array of %d elements\n", total_time, length_of_array)
+	fmt.Printf("sort took %s to sort array of %d elements\n", totalTime, lengthOfArray)
 }
